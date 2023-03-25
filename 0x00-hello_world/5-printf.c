10a19f0f8507e141f6203109826b0ffc80bfbc9c
collo-kihara <collokihara@gmail.com>
2023-03-25 20:13:20 +0300

    script-0_6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - this is the main fucntion
+*
+* Return; 0 when successfull
+*/
+
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n")
+    return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main fucntion
+ * 
+ * Return; 0 if successfull
+*/
+int main(void)
+{
+    printf("size of char; %lu byte(s)", sizeof(char)\n)
+    printf("size of int; %lu byte(s)", sizeof(int)\n)
+    printf("size of a long int; %lu byte(s)", sizeof(long int)\n)
+    printf("size of long long int; %lu byte(s)"; sizeof(long long int)\n)
+    printf("size of float; %lu byte(s)"; sizeof(float)\n)
+    return (0);
+}
