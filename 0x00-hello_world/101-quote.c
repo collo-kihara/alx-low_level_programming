6c39af2b914384bac5c36a381eee1f6ca04be9f9
collo-kihara <collokihara@gmail.com>
2023-03-26 17:29:03 +0300

    advanced tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - this is the main function
+ * 
+ * Return: 1 if successful
+*/
+
+int main(void)
+{
+    fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+    return (1);
+}
+
