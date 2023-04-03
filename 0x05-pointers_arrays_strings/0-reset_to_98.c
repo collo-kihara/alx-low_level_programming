aa6132a36c9d53dc9b08552792c4affc9a917279
collo-kihara <collokihara@gmail.com>
2023-04-04 00:22:51 +0300

    reset_to_98-fix

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - takes a pointer to an int as parameter and updates the value it points to to 98
+ * reset_to_98 - takes pointer to int as parameter and updates value to 98
  *
  * @n: parameter to be replaced
  *
